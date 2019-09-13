7f6264b2bae947a8cba7f5d9b31981fc8c02f354
Katherine Grey <katherine.grey@smartebusiness.co.uk>
2019-09-13 13:16:38 +0100

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker build -t kitkatlite/multi-client:latest -t kitkatlite/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t kitkatlite/multi-server:latest -t kitkatlite/multi-worker:$SHA -f ./server/Dockerfile ./server
+docker build -t kitkatlite/multi-worker:latest -t kitkatlite/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push kitkatlite/multi-client:latest
+docker push kitkatlite/multi-server:latest
+docker push kitkatlite/multi-worker:latest
+
+docker push kitkatlite/multi-client:$SHA
+docker push kitkatlite/multi-server:$SHA
+docker push kitkatlite/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl rollout restart deployment/server-deployment
+kubectl rollout restart deployment/client-deployment
+kubectl rollout restart deployment/worker-deployment
